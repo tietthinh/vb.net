d153e5d9fa914c4958660d2aa42c6cc26e41d480
Dương Tấn Huỳnh Phong <aphamagnetwarrior@yahoo.com>
2015-11-14 12:10:11 +0700

    Update Database/QLNhaHang.eap(

---
+++ QuanLyNhaHang/QuanLyNhaHang/DatabaseConnection.vb
@@ -0,0 +1,74 @@
+﻿Public Class DatabaseConnection
+    Private _Connecter As New SqlClient.SqlConnection()
+    Private Shared _Address As String = "Data Source=(LocalDB)\v11.0;AttachDbFilename=" + Environment.CurrentDirectory + "\QuanLyQuanAn.mdf;Integrated Security=True"
+
+    Public Property Connecter() As SqlClient.SqlConnection
+        Get
+            Return _Connecter
+        End Get
+        Set(value As SqlClient.SqlConnection)
+            _Connecter = value
+        End Set
+    End Property
+
+    Public Sub New()
+        _Connecter.ConnectionString = _Address
+    End Sub
+
+    Public Function Open() As Boolean
+        Try
+            _Connecter.Open()
+        Catch
+            Return False
+        End Try
+        Return True
+    End Function
+    Public Function Close() As Boolean
+        Try
+            _Connecter.Close()
+        Catch
+            Return False
+        End Try
+        Return True
+    End Function
+    Public Function Query(Optional _query = "Select * From Account") As DataTable
+        Dim temp As New DataTable()
+        Dim cmd As New SqlClient.SqlCommand(_query, Connecter)
+        Dim adt As New SqlClient.SqlDataAdapter(cmd)
+        adt.Fill(temp)
+        cmd.Dispose()
+        cmd = Nothing
+        adt.Dispose()
+        adt = Nothing
+        Return temp
+    End Function
+    Public Sub Update(ByVal table As DataTable, ByVal tableName As String)
+        Dim cmd As SqlClient.SqlCommand = New SqlClient.SqlCommand()
+        cmd.Connection = Me.Connecter
+        cmd.CommandText = "Select * From " + tableName
+        Dim adt As SqlClient.SqlDataAdapter = New SqlClient.SqlDataAdapter()
+        adt.SelectCommand = cmd
+        Dim builder As SqlClient.SqlCommandBuilder = New SqlClient.SqlCommandBuilder(adt)
+        adt.Update(table)
+        Me.Close()
+    End Sub
+
+    Public Function CheckInfor(ByVal userName As String, ByVal pass As String, ByVal confirmPass As String, _
+                               ByVal dateTime As DateTime) As Integer
+        Dim temp As New DataTable()
+        Me.Open()
+        temp = Me.Query()
+        For Each row As DataRow In temp.Rows
+            If (userName = row("Username")) Then
+                Return 1
+            End If
+        Next
+        If (confirmPass <> pass) Then
+            Return 2
+        End If
+        If (dateTime > Now) Then
+            Return 3
+        End If
+        Return -1
+    End Function
+End Class
--- QuanLyNhaHang/QuanLyNhaHang/frmMain.vb
+++ QuanLyNhaHang/QuanLyNhaHang/frmMain.vb
@@ -1,7 +1,14 @@
 ﻿Public Class frmMain
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        tsmLogout.Visible = False
+        Dim frmLogin As New frmDangNhap()
+        Dim result As DialogResult = frmLogin.ShowDialog()
+        If result = Windows.Forms.DialogResult.Cancel Then
+            Me.Close()
+        Else
+        End If
+        tsmLogout.Visible = True
+        tsmLogin.Visible = False
         tsmFunction.Visible = False
     End Sub
 End Class
