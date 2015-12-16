5a2f3390bda7c0ced67b6f136b506a77fab0d010
Dương Tấn Huỳnh Phong <aphamagnetwarrior@yahoo.com>
2015-12-16 15:21:42 +0700

    SAVEPOINT

--- QuanLyNhaHang/Chef/frmChef.vb
+++ QuanLyNhaHang/Chef/frmChef.vb
@@ -10,7 +10,7 @@ Public Class frmChef
 
     Dim db As New DatabaseConnection()
     Dim orderList As DataTable
-    Dim cookList As DataTable
+    Dim cookList As New DataTable()
     Dim cantServeList As DataTable
     Dim materialList As DataTable
     Dim currentDish As String
@@ -21,7 +21,7 @@ Public Class frmChef
         parameter.Value = "12:25:31.4409220"
         Try
             db.Open()
-            orderList = db.Query("Select DSDMTN.MaChuyen, DSDMTN.MaMon, DSDMTN.ThoiGian, DSDMTN.SoLuong, DSDMTN.GhiChu, MADU.TenMon From DanhSachDatMonTrongNgay DSDMTN, MonAnDoUong MADU Where DSDMTN.MaMon = MADU.MaMon")
+            orderList = db.Query("Select DSDMTN.MaChuyen, DSDMTN.MaMon, MADU.TenMon, DSDMTN.ThoiGian, DSDMTN.SoLuong, DSDMTN.GhiChu From DanhSachDatMonTrongNgay DSDMTN, MonAnDoUong MADU Where DSDMTN.MaMon = MADU.MaMon")
         Catch ex As Exception
             Throw ex
         Finally
@@ -30,12 +30,12 @@ Public Class frmChef
         Dim index As Integer = 1
         For Each row As DataRow In orderList.Rows
             Dim item As New ListViewItem(index)
-            item.SubItems.Add(row(0))
-            item.SubItems.Add(row(1))
+            item.SubItems.Add(row("MaChuyen"))
+            item.SubItems.Add(row("MaMon"))
             item.SubItems.Add(row("TenMon"))
-            item.SubItems.Add(row(2).ToString())
-            item.SubItems.Add(row(3))
-            item.SubItems.Add(row(4).ToString())
+            item.SubItems.Add(row("ThoiGian").ToString())
+            item.SubItems.Add(row("SoLuong"))
+            item.SubItems.Add(row("GhiChu").ToString())
             item.SubItems(0).Name = "STT"
             item.SubItems(1).Name = "MaChuyen"
             item.SubItems(2).Name = "MaMon"
@@ -46,11 +46,16 @@ Public Class frmChef
             ltvOrderList.Items.Add(item)
             index += 1
         Next
+
         ltvOrderList.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize)
         ltvOrderList.AutoResizeColumn(3, ColumnHeaderAutoResizeStyle.ColumnContent)
         ltvOrderList.AutoResizeColumn(4, ColumnHeaderAutoResizeStyle.ColumnContent)
         ltvOrderList.AutoResizeColumn(5, ColumnHeaderAutoResizeStyle.HeaderSize)
-        cookList = orderList.Clone()
+
+        cookList.Columns.Add(New DataColumn(orderList.Columns("MaChuyen").ColumnName, orderList.Columns("MaChuyen").DataType))
+        cookList.Columns.Add(New DataColumn(orderList.Columns("TenMon").ColumnName, orderList.Columns("TenMon").DataType))
+        cookList.Columns.Add(New DataColumn(orderList.Columns("SoLuong").ColumnName, orderList.Columns("SoLuong").DataType))
+
         cantServeList = orderList.Clone()
     End Sub
 
@@ -83,6 +88,23 @@ Public Class frmChef
     End Sub
 
     Private Sub btnCook_Click(sender As Object, e As EventArgs) Handles btnCook.Click
+        If ltvOrderList.SelectedItems.Count > 0 Then
+            For Each item As ListViewItem In ltvOrderList.SelectedItems
+                Dim row As DataRow = cookList.NewRow
+                row("MaChuyen") = item.SubItems("MaChuyen").Text
+                row("TenMon") = item.SubItems("TenMon").Text
+                row("SoLuong") = item.SubItems("SoLuong").Text
+                cookList.Rows.Add(row)
+            Next
 
+            dgvCookList.DataSource = cookList
+
+            For i As Integer = ltvOrderList.SelectedItems.Count - 1 To 0 Step -1
+                orderList.Rows(ltvOrderList.SelectedItems(i).Index).Delete()
+                ltvOrderList.SelectedItems(i).Remove()
+            Next
+
+            txtTotalQuantity.Text = ""
+        End If
     End Sub
 End Class
