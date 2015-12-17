e0d873a151f64116e0cb21318bef6a8c85f55173
Dương Tấn Huỳnh Phong <aphamagnetwarrior@yahoo.com>
2015-12-17 19:42:06 +0700

    Fix login and change name to frmLogin

--- QuanLyNhaHang/Library/frmLogin.Designer.vb
+++ QuanLyNhaHang/Library/frmLogin.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmDangNhap
+Partial Class frmLogin
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
